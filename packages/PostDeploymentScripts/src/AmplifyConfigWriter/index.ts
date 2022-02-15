92a5c5bf02a6ae0213fd204e06c688ec6dc7b4bb
Elgin Lam <elginlam@amazon.com>
2022-02-15 11:31:49 +1100

    chore: bump version numbers

--- packages/PostDeploymentScripts/src/AmplifyConfigWriter/index.ts
+++ packages/PostDeploymentScripts/src/AmplifyConfigWriter/index.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import cdkOutput from '../output.json'
+import { S3Client, PutObjectCommand,PutObjectCommandInput } from "@aws-sdk/client-s3";
+
 
 interface IConfig {
     region?: string,
@@ -36,6 +38,7 @@ export default class AmplifyConfigWriter  {
                 configOutput.dataApiEndpoint = value
             else if (key==='DeploymentRegion')
                 configOutput.region = value
+          
         }
 
         const absPath = path.resolve('../ui/src/config.json')
